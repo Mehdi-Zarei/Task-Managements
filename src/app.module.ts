189a6149ce31e09b3c0b201ff032e6030a1bf10c
Mehdi <mahdizareiofficial@gmail.com>
2025-05-30 17:57:32 +0330

    feat: add TypeORM and PostgreSQL integration with initial module imports
    - Added @nestjs/mapped-types and @nestjs/typeorm dependencies to package.json.
    - Configured TypeOrmModule in app.module.ts for PostgreSQL connection.
    - Imported UserModule, ProjectModule, TaskModule, and CategoryModule into the main application module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,26 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ProjectModule } from './project/project.module';
+import { TaskModule } from './task/task.module';
+import { CategoryModule } from './category/category.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'Task-Management',
+      username: 'postgres',
+      password: '123',
+      synchronize: true,
+      entities: [__dirname + '/**/entities/*.entity.{ts,js}'],
+    }),
+    UserModule,
+    ProjectModule,
+    TaskModule,
+    CategoryModule,
+  ],
 })
 export class AppModule {}
